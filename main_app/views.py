cf4cef087b5555d66cd2887837ff180c107fd55a
Jessica Vinluan <jessicavinluan@Jessicas-MacBook-Air.local>
2022-04-06 22:19:07 -0400

    initial commit

---
+++ main_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here:
+
+# Add the following import
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+def about(request):
+    return render(request, 'about.html')
