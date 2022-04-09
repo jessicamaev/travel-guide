7532091573291cdbb1c8efdc20f192a5e54fc930
ani0104 <annie.samajpati@gmail.com>
2022-04-09 14:14:41 -0400

    first commit

--- main_app/views.py
+++ main_app/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 
 # Define the home view
 def home(request):
-    return HttpResponse('<h1>Well Hello /ᐠ｡‸｡ᐟ\ﾉ there</h1>')
+    return HttpResponse('<h1>Well Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
 
 def about(request):
     return render(request, 'about.html')
