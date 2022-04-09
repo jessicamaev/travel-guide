6beb3c6128384caed4aa3bf02d53065635831013
Jessica Vinluan <jessicavinluan@Jessicas-MacBook-Air.local>
2022-04-09 16:58:53 -0400

    adding experience template and view

--- main_app/views.py
+++ main_app/views.py
@@ -6,7 +6,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Define the home view
-def home(request):
+def home(request): 
     return HttpResponse('<h1>made a change here Hello here it is /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
 def about(request):
     return render(request, 'about.html')
+
+def experiences(request):
+    return render(request, 'experiences.html')
