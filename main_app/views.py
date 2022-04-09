e56dd414c0e5011fe62894df512f63de99323dcc
Jessica Vinluan <jessicavinluan@Jessicas-MacBook-Air.local>
2022-04-09 14:22:08 -0400

    made a change to hello in views

--- main_app/views.py
+++ main_app/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 
 # Define the home view
 def home(request):
-    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return HttpResponse('<h1>made a change here Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
 
 def about(request):
     return render(request, 'about.html')
