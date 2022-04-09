095cbd79e056d03ad9eaa7e0f3f26d9ca642b435
Jessica Vinluan <jessicavinluan@Jessicas-MacBook-Air.local>
2022-04-09 15:06:43 -0400

    changes

--- main_app/views.py
+++ main_app/views.py
@@ -8,6 +8,5 @@ from django.http import HttpResponse
 # Define the home view
 def home(request):
     return HttpResponse('<h1>made a change here Hello here it is /ᐠ｡‸｡ᐟ\ﾉ</h1>')
-
 def about(request):
     return render(request, 'about.html')
